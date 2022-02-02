8cdccfb8b008ea59df65c2152f9394a384a33990
ChrisWestesson <chris.westesson@gmail.com>
2022-02-02 15:34:29 +0100

    Lagt till fragments

---
+++ app/src/main/java/com/christianwestesson/vaxtvakten/AddUnlistedPlanFragment.kt
@@ -0,0 +1,28 @@
+package com.christianwestesson.vaxtvakten
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class AddUnlistedPlanFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_unlisted_plan, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
--- app/src/main/java/com/christianwestesson/vaxtvakten/MainActivity.kt
+++ app/src/main/java/com/christianwestesson/vaxtvakten/MainActivity.kt
@@ -2,11 +2,28 @@ package com.christianwestesson.vaxtvakten
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.fragment.app.FragmentContainerView
 
 class MainActivity : AppCompatActivity() {
+
+    val homeFrag = HomeFragment()
+    val addPlantList = AddPlantListFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction().replace(R.id.fragContainer, homeFrag).commit()
+        //val fragcont = findViewById<FragmentContainerView>(R.id.fragContainer)
+
+        findViewById<Button>(R.id.mainActivityGoAddPlantListBtn).setOnClickListener {
+            supportFragmentManager.beginTransaction().replace(R.id.fragContainer, addPlantList).commit()
+        }
+
+        findViewById<Button>(R.id.mainActivityGoHomeBtn).setOnClickListener {
+            supportFragmentManager.beginTransaction().replace(R.id.fragContainer, homeFrag).commit()
+        }
 
         //test github
         // Mattias was here
